93c68932a477fbe0584d498e2e570ba073758fb6
Divyansh <divyansh2375@gmail.com>
2023-07-06 10:52:43 +0530

    basic layout design

--- app.js
+++ app.js
@@ -1,50 +1,69 @@
-// const floorValue = document.querySelector("#floorNumber")
+const floorValue = document.querySelector("#floorNumber")
 const liftValue = document.querySelector("#liftNumber")
+const simulate = document.querySelector(".createLiftFloorButton");
+const floorContainer = document.querySelector("#adding-floor")
 
+simulate.addEventListener("click", function () {
+  // 1.Try
+  // console.log("your Floor no is =" +floorValue.value )
+  // console.log("your Lift no is =" + liftValue.value )
 
-const simulate = document.querySelector(".createLiftFloorButton")
+  // 2.
+  // const floorCreate = document.createElement("div")
+  // floorCreate.id = "floor"
 
-simulate.addEventListener('click', function(){
+  // document.body.appendChild(floorCreate)
 
-    // 1.Try  
-    // console.log("your Floor no is =" +floorValue.value )
-    // console.log("your Lift no is =" + liftValue.value )
+  // 3.
+  const floorshow = parseInt(floorValue.value) //4
+  const liftshow = parseInt(liftValue.value)
+    const inputfloor = floorshow
+    const inputLift = liftshow
+    if (inputfloor<inputLift){
+        alert("floor cannot be less than lift")
+        return;
+    }
+    if (inputLift <= 0 || inputfloor <= 0){
+        alert("please enter right data")
+        return;
+    }else
+    {
 
-    
-    
-    // 2.
-    // const floorCreate = document.createElement("div")
-    // floorCreate.id = "floor"
+      
+      for (let i = 0 ; i<floorshow ;i++){
 
-    // document.body.appendChild(floorCreate)
-    
-
-    // 3.
-    const floorValue = document.querySelector("#floorNumber")
-    const floorshow = parseInt(floorValue.value)   //4
-    
-    for (let i = 0 ; i< floorshow; i++){
+        const floorCreate = document.createElement("div");
+        floorCreate.className = "floor";
         
+        const liftCreate = document.createElement("div")
+        liftCreate.className = "lift"
+        
+        const floorName = document.createElement("p");
 
+      floorName.textContent = " Floor: " + `${floorshow - i}`; //4-2-0
 
-        const floorCreate = document.createElement("div")
-        floorCreate.className = "floor"
-        const floorName = document.createElement("p")
- 
-        floorName.textContent = " Floor: " + `${floorshow-i}` //4-2-0
-
-        floorName.className = "floorCounting"
-
-        floorCreate.appendChild(floorName)
-
-        document.body.appendChild(floorCreate)
-
-        console.log(floorCreate.innerHTML)
-
+      floorName.className = "floorCounting";
+      
+      
+      floorCreate.appendChild(floorName);
+      floorCreate.appendChild(liftCreate);
+      document.body.appendChild(floorCreate);
+      
     }
 
+  
+    }
+      
+    }
+    
 
+    
 
 
     
-})
+
+  
+  
+
+
+);
